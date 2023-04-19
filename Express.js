2a19a6101ff9c23380557a156da9b0b07096c734
Your GitHub Username <samholbrook777@gmail.com>
2023-04-19 03:41:56 +0100

    Added delete account button and organisation roles

---
+++ Express.js
@@ -0,0 +1,348 @@
+const express = require("express");
+const path = require('path');
+const router = express.Router();
+const mysql = require("mysql");
+const cookieParser = require("cookie-parser");
+const axios = require('axios'); 
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+const upload = multer({ storage: storage });
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "chat_app",
+});
+
+function checkAuth(req, res, next) {
+  if (!req.cookies.user_id) {
+    res.redirect("/login");
+  } else {
+    next();
+  }
+}
+
+router.get("/messages", checkAuth, function (req, res) {
+  const user_id = req.cookies.user_id;
+
+  connection.query(
+    "SELECT users.id as friend_id, users.username as friend_name FROM friendships JOIN users ON friendships.friend_id = users.id WHERE friendships.user_id = ? AND friendships.status = ?",
+    [user_id, "accepted"],
+    function (err, friends, fields) {
+      if (err) throw err;
+
+      if (friends.length > 0) {
+        const firstFriendId = friends[0].friend_id;
+        res.redirect(`/messages/${firstFriendId}`);
+      } else {
+        res.render("messages", {
+          messages: [],
+          user_id: user_id,
+          friend_id: null,
+          friends: friends,
+        });
+      }
+    }
+  );
+});
+
+router.post("/messages", checkAuth, upload.single('file'), function (req, res) {
+  const user_id = req.cookies.user_id;
+  const friend_id = req.body.friend_id;
+  const message = req.body.message;
+
+  const file = req.file;
+  let filePath = null;
+  if (file) {
+    filePath = path.join("uploads", file.filename);
+  }
+
+  if (!message && !file) {
+    res.redirect("/messages/" + friend_id);
+    return;
+  }
+
+  connection.query(
+    "INSERT INTO friend_chats (sender_id, receiver_id, message, file_path, original_filename) VALUES (?, ?, ?, ?, ?)",
+    [user_id, friend_id, message || null, filePath, file ? file.originalname : null],
+    function (err, result) {
+      if (err) throw err;
+
+      res.redirect("/messages/" + friend_id);
+    }
+  );
+});
+
+router.get("/messages/:friend_id", checkAuth, function (req, res) {
+  const user_id = req.cookies.user_id;
+  const friend_id = req.params.friend_id;
+
+  connection.query(
+    "SELECT users.id as friend_id, users.username as friend_name FROM friendships JOIN users ON friendships.friend_id = users.id WHERE friendships.user_id = ? AND friendships.status = ?",
+    [user_id, "accepted"],
+    function (err, friends, fields) {
+      if (err) throw err;
+
+      connection.query(
+        "SELECT friend_chats.*, users.username as sender FROM friend_chats JOIN users ON friend_chats.sender_id = users.id WHERE (sender_id = ? AND receiver_id = ?) OR (sender_id = ? AND receiver_id = ?) ORDER BY timestamp ASC",
+        [user_id, friend_id, friend_id, user_id],
+        function (err, messages, fields) {
+          if (err) throw err;
+
+          res.render("messages", {
+            messages: messages,
+            user_id: user_id,
+            friend_id: friend_id,
+            friends: friends,
+          });
+        }
+      );
+    }
+  );
+});
+
+
+router.get("/download/:message_id", checkAuth, function (req, res) {
+  const message_id = req.params.message_id;
+
+  connection.query(
+    "SELECT * FROM friend_chats WHERE id = ?",
+    [message_id],
+    function (err, results) {
+      if (err) throw err;
+
+      if (results.length > 0) {
+        const file_path = results[0].file_path;
+        const original_filename = results[0].original_filename;
+
+        if (file_path) {
+          res.download(path.join(__dirname, file_path), original_filename, function (err) {
+            if (err) {
+              console.error("Error downloading the file:", err);
+              res.status(500).send("Failed to download the file");
+            }
+          });
+        } else {
+          res.status(404).send("No file found");
+        }
+      } else {
+        res.status(404).send("Message not found");
+      }
+    }
+  );
+});
+
+
+router.get("/api/messages/:friend_id", checkAuth, function (req, res) {
+  const user_id = req.cookies.user_id;
+  const friend_id = req.params.friend_id;
+
+  connection.query(
+    "SELECT friend_chats.*, users.username as sender FROM friend_chats JOIN users ON friend_chats.sender_id = users.id WHERE (sender_id = ? AND receiver_id = ?) OR (sender_id = ? AND receiver_id = ?) ORDER BY timestamp ASC",
+    [user_id, friend_id, friend_id, user_id],
+    function (err, messages, fields) {
+      if (err) throw err;
+
+      res.json(messages);
+    }
+  );
+});
+
+router.get("/friends", checkAuth, function (req, res) {
+  const user_id = req.cookies.user_id;
+
+  connection.query(
+    "SELECT users.* FROM friendships JOIN users ON friendships.friend_id = users.id WHERE friendships.user_id = ? AND friendships.status = ?",
+    [user_id, "accepted"],
+    function (err, friends, fields) {
+      if (err) throw err;
+
+      connection.query(
+        "SELECT users.* FROM friendships JOIN users ON friendships.user_id = users.id WHERE friendships.friend_id = ? AND friendships.status = ?",
+        [user_id, "pending"],
+        function (err, requests, fields) {
+          if (err) throw err;
+
+          res.render("friends", {
+            friends: friends,
+            requests: requests,
+            user_id: user_id,
+            error: "",
+            searchResults: [], 
+          });
+        }
+      );
+    }
+  );
+});
+
+router.post("/friends/search", checkAuth, function (req, res) {
+  const user_id = req.cookies.user_id;
+  const query = req.body.query;
+
+  connection.query(
+    `SELECT username, id FROM users WHERE username LIKE '%${query}%' AND id <> ?`,
+    [user_id],
+    function (err, searchResults, fields) {
+      if (err) throw err;
+
+      connection.query(
+        "SELECT users.* FROM friendships JOIN users ON friendships.friend_id = users.id WHERE friendships.user_id = ? AND friendships.status = ?",
+        [user_id, "accepted"],
+        function (err, friends, fields) {
+          if (err) throw err;
+
+          connection.query(
+            "SELECT users.* FROM friendships JOIN users ON friendships.user_id = users.id WHERE friendships.friend_id = ? AND friendships.status = ?",
+            [user_id, "pending"],
+            function (err, requests, fields) {
+              if (err) throw err;
+
+              res.render("friends", {
+                friends: friends,
+                requests: requests,
+                searchResults: searchResults,
+                error: "",
+              });
+            }
+          );
+        }
+      );
+    }
+  );
+});
+
+router.post("/friends/send-request", checkAuth, function (req, res) {
+  const user_id = req.cookies.user_id;
+  const friend_id = req.body.friend_id;
+
+  connection.query(
+    "SELECT * FROM friendships WHERE (user_id = ? AND friend_id = ?) OR (user_id = ? AND friend_id = ?)",
+    [user_id, friend_id, friend_id, user_id],
+    function (err, results) {
+      if (err) throw err;
+
+      if (results.length > 0) {
+        res.render("friends", {
+          error: "A friend request is already pending.",
+          friends: [],
+          requests: [], 
+          searchResults: [],
+        });
+      } else {
+        connection.query(
+          "INSERT INTO friendships (user_id, friend_id, status) VALUES (?, ?, ?)",
+          [user_id, friend_id, "pending"],
+          function (err, result) {
+            if (err) throw err;
+
+            res.redirect("/friends");
+          }
+        );
+      }
+    }
+  );
+});
+
+router.post("/friends/accept-request", checkAuth, function (req, res) {
+  const user_id = req.cookies.user_id;
+  const request_id = req.body.request_id;
+
+  connection.query(
+    "UPDATE friendships SET status = ? WHERE user_id = ? AND friend_id = ?",
+    ["accepted", request_id, user_id],
+    function (err, result) {
+      if (err) throw err;
+
+      connection.query(
+        "INSERT INTO friendships (user_id, friend_id, status) VALUES (?, ?, ?)",
+        [user_id, request_id, "accepted"],
+        function (err, result) {
+          if (err) throw err;
+
+          res.redirect("/friends");
+        }
+      );
+    }
+  );
+});
+
+router.post("/friends/decline-request", checkAuth, function (req, res) {
+  const user_id = req.cookies.user_id;
+  const request_id = req.body.request_id;
+
+  connection.query(
+    "UPDATE friendships SET status = ? WHERE user_id = ? AND friend_id = ?",
+    ["declined", request_id, user_id],
+    function (err, result) {
+      if (err) throw err;
+
+      res.redirect("/friends");
+    }
+  );
+});
+
+router.get("/account", checkAuth, function (req, res) {
+  const user_id = req.cookies.user_id;
+
+  connection.query(
+    "SELECT * FROM users WHERE id = ?",
+    [user_id],
+    function (err, rows, fields) {
+      if (err) throw err;
+
+      const user = rows[0]; 
+
+      res.render("account", { user: user });
+    }
+  );
+});
+
+// Delete account and remove friendships
+router.post("/delete-account", checkAuth, function (req, res) {
+  const user_id = req.cookies.user_id;
+
+  connection.query(
+    "SELECT * FROM friendships WHERE user_id = ? OR friend_id = ?",
+    [user_id, user_id],
+    function (err, rows, fields) {
+      if (err) throw err;
+
+      // Remove any friendships involving the user
+      for (let i = 0; i < rows.length; i++) {
+        const friendship = rows[i];
+
+        connection.query(
+          "DELETE FROM friendships WHERE id = ?",
+          [friendship.id],
+          function (err, result) {
+            if (err) throw err;
+          }
+        );
+      }
+
+      // Delete the user's account
+      connection.query(
+        "DELETE FROM users WHERE id = ?",
+        [user_id],
+        function (err, result) {
+          if (err) throw err;
+
+          // Clear the user_id cookie and redirect to the login page
+          res.clearCookie("user_id");
+          res.redirect("/login");
+        }
+      );
+    }
+  );
+});
+
+module.exports = router;
---
+++ README.md
@@ -0,0 +1,60 @@
+# frogPrince
+
+## Steps to set up
+
+Download vs code: https://code.visualstudio.com/download 
+
+Download node.js: https://nodejs.org/en/download/
+
+Download xampp: https://www.apachefriends.org/download.html
+start apache server and mysql.
+
+Click admin button next on mysql(Windows) or enter "localhost/phpmyadmin/"(Mac). First click new on the list of databases on the left of the page and do not change any of the settings for set up but just name the database "chat_app". 
+Create a new database by clicking on the "New" button on the left-hand side and giving it a name.
+Select the new database from the left-hand sidebar and click on the "Import" tab at the top.
+Click on the "Choose File" button and select the SQL dump file you provided.
+Click on the "Go" button at the bottom to import the database.Then inside the DB go to sql tab and enter the following sql commands. 
+
+For the users table: "CREATE TABLE users (
+  id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  Email VARCHAR(255),
+  organization VARCHAR(255)
+);"
+
+For the messages table: "CREATE TABLE messages (
+  id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  message VARCHAR(255) NOT NULL,
+  timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  user_id INT(11)
+);"
+
+For the friend list table: "CREATE TABLE `friendships` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `friend_id` int(11) NOT NULL,
+  `status` ENUM('pending', 'accepted', 'declined') NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (friend_id) REFERENCES users(id)
+);"
+
+For the chatting with friends table: "CREATE TABLE friend_chats (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  sender_id INT NOT NULL,
+  receiver_id INT NOT NULL,
+  message TEXT NOT NULL,
+  timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);"
+
+
+Inside terminal with project open enter "npm install". This should install all packages based off what is inside the packages folder that you installed.
+
+Click new terminal in vscode and enter "nodemon app.js". The nodemon command might not work on Mac so please use "npm app.js" instead. Using nodemon instead of node allows for server to automatically restart when any change are saved
+
+Go to browser and enter "localhost:3000/login"
+
+## Information
+
+Views folder holds html files that are ejs file format (embedded javascript), public/css folder is all the css files. Main javascript files are in main folder.
