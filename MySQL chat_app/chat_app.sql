5165df8dc58d3b22b89834595d12762c73e77a6e
Your GitHub Username <samholbrook777@gmail.com>
2023-04-25 04:21:08 +0100

    added updated export sql file for chat_app

--- MySQL chat_app/chat_app.sql
+++ MySQL chat_app/chat_app.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2023 at 09:33 PM
+-- Generation Time: Apr 25, 2023 at 05:20 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -31,19 +31,22 @@ CREATE TABLE `friendships` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `friend_id` int(11) NOT NULL,
-  `status` enum('pending','accepted','declined') NOT NULL
+  `status` enum('pending','accepted','declined') NOT NULL,
+  `is_colleague` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `friendships`
 --
 
-INSERT INTO `friendships` (`id`, `user_id`, `friend_id`, `status`) VALUES
-(3, 4, 3, 'accepted'),
-(4, 3, 4, 'accepted'),
-(5, 5, 4, 'accepted'),
-(6, 5, 3, 'pending'),
-(7, 4, 5, 'accepted');
+INSERT INTO `friendships` (`id`, `user_id`, `friend_id`, `status`, `is_colleague`) VALUES
+(3, 4, 3, 'accepted', NULL),
+(4, 3, 4, 'accepted', NULL),
+(5, 5, 4, 'accepted', NULL),
+(6, 5, 3, 'pending', NULL),
+(7, 4, 5, 'accepted', NULL),
+(14, 8, 4, 'accepted', 1),
+(15, 4, 8, 'accepted', 1);
 
 -- --------------------------------------------------------
 
@@ -68,7 +71,14 @@ CREATE TABLE `friend_chats` (
 INSERT INTO `friend_chats` (`id`, `sender_id`, `receiver_id`, `message`, `timestamp`, `original_filename`, `file_path`) VALUES
 (1, 2, 3, 'Hey', '2023-04-19 02:30:16', NULL, NULL),
 (2, 2, 3, NULL, '2023-04-19 02:31:50', 'Alfresco_Federation_Services_-_Datasheet.pdf', 'uploads\\Alfresco_Federation_Services_-_Datasheet.pdf'),
-(3, 4, 3, 'Hey', '2023-04-19 02:47:47', NULL, NULL);
+(3, 4, 3, 'Hey', '2023-04-19 02:47:47', NULL, NULL),
+(4, 4, 3, 'Hey', '2023-04-25 02:14:38', NULL, NULL),
+(5, 7, 4, 'Hey', '2023-04-25 02:42:51', NULL, NULL),
+(6, 7, 4, 'Hi', '2023-04-25 02:42:58', NULL, NULL),
+(7, 4, 7, 'Hey', '2023-04-25 02:54:42', NULL, NULL),
+(8, 4, 3, 'Hi', '2023-04-25 02:59:35', NULL, NULL),
+(9, 4, 5, 'Hey', '2023-04-25 02:59:43', NULL, NULL),
+(10, 4, 5, 'Hey', '2023-04-25 03:18:45', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -92,7 +102,8 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `username`, `password`, `Email`, `organization`, `role`) VALUES
 (3, 'test', 'test', 'test@gmail.com', 'test', 'Solutions consultant'),
 (4, 'demo', 'demo', 'demo@demo', 'demo', 'Solutions consultant'),
-(5, 'uwe', 'uwe', 'uwe@uwe.com', 'uwe', 'uwe');
+(5, 'uwe', 'uwe', 'uwe@uwe.com', 'uwe', 'uwe'),
+(8, 'demo5', 'demo', 'demo5@demo5', 'demo', 'Sales manager');
 
 --
 -- Indexes for dumped tables
@@ -126,19 +137,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `friendships`
 --
 ALTER TABLE `friendships`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `friend_chats`
 --
 ALTER TABLE `friend_chats`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Constraints for dumped tables
