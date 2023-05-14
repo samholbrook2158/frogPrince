639459045865648f108f0d4f26f4578e61318455
Your GitHub Username <samholbrook777@gmail.com>
2023-05-14 09:07:17 +0100

    null data fix for no renewal

--- public/dashboard.js
+++ public/dashboard.js
@@ -14,53 +14,66 @@ function displayRenewals() {
   const finishedRenewals = document.getElementById('finished-renewals-body');
   const activityTableBody = document.getElementById('activity-table-body');
 
-  const today = new Date().toISOString().split('T')[0];
+  // Clear the existing content
+  ongoingRenewals.innerHTML = '';
+  upcomingRenewals.innerHTML = '';
+  finishedRenewals.innerHTML = '';
+  activityTableBody.innerHTML = '';
 
   renewalData.forEach((renewal) => {
     const tableRow = document.createElement('tr');
 
     const productNameCell = document.createElement('td');
-    productNameCell.textContent = `Product Name: ${renewal.product_name}`;
+    productNameCell.textContent = renewal.product_name || '';
     tableRow.appendChild(productNameCell);
 
     const linkCell = document.createElement('td');
-    const linkElement = document.createElement('a');
-    linkElement.textContent = 'Link';
-    linkElement.href = `http://localhost:3000/messages/${renewal.friend_id}`;
-    linkElement.classList.add('renewal-link');
-    linkCell.appendChild(linkElement);
+
+    if (renewal.friend_id) {
+      const linkElement = document.createElement('a');
+      linkElement.textContent = 'Link';
+      linkElement.href = `http://localhost:3000/messages/${renewal.friend_id}`;
+      linkElement.classList.add('renewal-link');
+      linkCell.appendChild(linkElement);
+    }
+
     tableRow.appendChild(linkCell);
 
-
     if (renewal.status === 'Ongoing') {
-      ongoingRenewals.appendChild(tableRow.cloneNode(true));
+      ongoingRenewals.appendChild(tableRow);
     } else if (renewal.status === 'Upcoming') {
-      upcomingRenewals.appendChild(tableRow.cloneNode(true));
+      upcomingRenewals.appendChild(tableRow);
     } else if (renewal.status === 'Completed') {
-      finishedRenewals.appendChild(tableRow.cloneNode(true));
-    }        
+      finishedRenewals.appendChild(tableRow);
+    }
 
     // Update activity table
     const activityTableRow = document.createElement('tr');
 
     const activityTableCellName = document.createElement('td');
-    activityTableCellName.textContent = `${renewal.product_name}`;
+    activityTableCellName.textContent = renewal.product_name || '';
     activityTableRow.appendChild(activityTableCellName);
 
     const details = document.createElement('td');
-    details.textContent = `${renewal.contract_duration}`;
+    details.textContent = renewal.contract_duration || '';
     activityTableRow.appendChild(details);
 
     const activityTableCellStartDate = document.createElement('td');
-    activityTableCellStartDate.textContent = formatDate(renewal.start_date);
+    activityTableCellStartDate.textContent = renewal.start_date ? formatDate(renewal.start_date) : '';
     activityTableRow.appendChild(activityTableCellStartDate);
 
     const status = document.createElement('td');
-    status.textContent = renewal.status;
+    status.textContent = renewal.status || '';
     activityTableRow.appendChild(status);
 
     const activityTableCellLink = document.createElement('td');
-    activityTableCellLink.appendChild(linkElement.cloneNode(true));
+    if (renewal.friend_id) {
+      const linkElement = document.createElement('a');
+      linkElement.textContent = 'Link';
+      linkElement.href = `http://localhost:3000/messages/${renewal.friend_id}`;
+      linkElement.classList.add('renewal-link');
+      activityTableCellLink.appendChild(linkElement);
+    }
     activityTableRow.appendChild(activityTableCellLink);
 
     activityTableBody.appendChild(activityTableRow);
@@ -69,4 +82,3 @@ function displayRenewals() {
 
 // Call the displayRenewals function to update the dashboard
 displayRenewals();
-console.log(renewalData); // To check the actual data you're receiving
